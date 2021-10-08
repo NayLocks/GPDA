d87fa26c74977f4f3a90fbd023586c1a02cde3dc
j.delannoy <jonathan.delannoy@ribegroupe.com>
2021-10-08 12:20:24 +0200

    TEST

--- src/Controller/SettingsRetourController.php
+++ src/Controller/SettingsRetourController.php
@@ -46,8 +46,8 @@ class SettingsRetourController extends AbstractController
             $dompdf->setPaper('A4', 'portrait');
             $dompdf->render();
             $output = $dompdf->output();
-            $pdfFilepath =  'PDF/BR_'.$pda->getPNoSerial().'_'.$annee.'_'.$mois.'_'.$jour.".pdf";
-            $path =  "BR_".$pda->getPNoSerial().'_'.$annee.'_'.$mois.'_'.$jour.".pdf";
+            $pdfFilepath =  'http://ftp.ribegroupe.com/Appli/Gestion_PDA/Documents/BR/BR_'.$pda->getPNoSerial().'_'.$annee.'_'.$mois.'_'.$jour.".pdf";
+            $path =  "http://ftp.ribegroupe.com/Appli/Gestion_PDA/Documents/BR/BR_".$pda->getPNoSerial().'_'.$annee.'_'.$mois.'_'.$jour.".pdf";
             file_put_contents($pdfFilepath, $output);
 
             /*$allTypeDocuments = $this->getDoctrine()->getRepository(TypeDocuments::class);
